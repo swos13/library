c3e2679a0155d1f6d900b3009b4676fb2151d6c3
swos <szymon.wos1319@gmail.com>
2024-01-24 12:37:37 +0100

    Add book to library through form

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 const addButton = document.querySelector(".add");
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
+const pagesInput = document.querySelector("#pages-input");
+const libraryContainer = document.querySelector(".library-container");
 const library = [];
 
-function Book(name, author, pages){
-    this.name = name;
+function Book(title, author, pages){
+    this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-function addBookToLibrary(book){
-    library.push(book);
+function addBookToDOM(book){
+    let bookContainer = document.createElement("div");
+    bookContainer.classList.add("book-container");
+
+    let title = document.createElement("div");
+    let author = document.createElement("div");
+    let pages = document.createElement("div");
+    let deleteButton = document.createElement("button");
+
+    title.textContent = book.title;
+    title.classList.add("title");
+    author.textContent = book.author;
+    author.classList.add("author");
+    pages.textContent = book.pages;
+    pages.classList.add("pages");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete");
+
+    bookContainer.appendChild(title);
+    bookContainer.appendChild(author);
+    bookContainer.appendChild(pages);
+    bookContainer.appendChild(deleteButton);
+
+    libraryContainer.appendChild(bookContainer);
 }
 
-addButton.addEventListener("click", () => {
-    let form = document.createElement("form");
+function addBookToLibrary(title, author, pages){
+    let book = new Book(title, author, pages);
+    library.push(book);
+    addBookToDOM(book);
+}
+
+addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    addBookToLibrary(title, author, pages);
 })
